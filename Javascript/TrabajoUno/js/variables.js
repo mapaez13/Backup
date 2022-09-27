a17ce81c6b31a3414174d92c0b1f44cd8d75f078
mapaez13 <mapaez13@gmail.com>
2022-09-27 15:39:56 -0400

    modified:   Javascript/TrabajoUno/index.html 	modified:   Javascript/TrabajoUno/js/variables.js

--- Javascript/TrabajoUno/js/variables.js
+++ Javascript/TrabajoUno/js/variables.js
@@ -29,5 +29,11 @@ console.log("jugo",jugo);
 El DOM es la estructura de objetos genera el navegador
 */
 
-var caja=document.querySelector("");
+var caja=document.querySelector("#caja");
 console.log("caja: ",caja);
+caja.style.width="200px";
+caja.style.height="200px";
+caja.style.background="red";
+
+var cajas=document.querySelectorAll(".cajas");
+console.log("cajas: ",cajas);
