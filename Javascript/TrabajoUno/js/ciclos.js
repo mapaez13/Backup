833542421e70c586f666ef69efe3d8dcefdbe526
mapaez13 <mapaez13@gmail.com>
2022-10-10 08:09:15 -0300

    modified:   Javascript/TrabajoUno/js/ciclos.js 	modified:   Proyecto/assets/css/ptf-main.min.css 	modified:   Proyecto/assets/img/home/default/main-image.png 	modified:   Proyecto/assets/img/root/favicon.png 	modified:   Proyecto/index.html

--- Javascript/TrabajoUno/js/ciclos.js
+++ Javascript/TrabajoUno/js/ciclos.js
@@ -7,6 +7,7 @@ console.log(cajas[0])
 for(var i=0;i<cajas.length;i++){
     cajas[i].style.width="50px";
     cajas[i].style.height="50px";
-    cajas[i].style.background="rgb(48,67,33)";
+    cajas[i].style.background="rgb("+ i*20+","+i*40+","+i*60+")";
     cajas[i].style.margin="10px";
+    cajas[i].style.display="inline-block";
 }
