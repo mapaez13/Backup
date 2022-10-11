ee5270c4a96438b4212d990b41f7f1c2c757028a
mapaez13 <mapaez13@gmail.com>
2022-10-11 15:17:59 -0300

    modified:   Javascript/TrabajoUno/index.html 	new file:   Javascript/TrabajoUno/js/eventos.js 	modified:   Proyecto/assets/css/ptf-main.min.css 	modified:   Proyecto/assets/img/root/logo-dark.png 	modified:   Proyecto/index.html

---
+++ Javascript/TrabajoUno/js/eventos.js
@@ -0,0 +1,27 @@
+var cajita= document.querySelector("#cajita");
+cajita.style.width="200px";
+cajita.style.height="200px";
+cajita.style.background="rgb(25, 25, 112)";
+
+/*===================================================
+Eventos desde DOM
+===================================================*/
+var recuadro=document.querySelector(".recuadro");
+
+function cambiarColor(){
+    recuadro.style.background="red";
+}
+
+function restaurarColor(){
+    recuadro.style.background="darkgreen";
+}
+
+var boton= document.querySelector("#boton");
+boton.addEventListener("click",moverCaja)
+
+
+function moverCaja(){
+    recuadro.style.width="500px";
+    recuadro.style.transition="10s";
+}
+
