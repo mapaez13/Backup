762bb0a00beee3fb7c0fe929b08f1415e8bfa00b
mapaez13 <mapaez13@gmail.com>
2022-10-26 10:58:52 -0300

    modified:   Javascript/Tarea Tres/js/main.js 	modified:   Javascript/Tarea Tres/tres.html

--- Javascript/Tarea Tres/js/main.js
+++ Javascript/Tarea Tres/js/main.js
@@ -1,5 +1,12 @@
-let img= document.getElementById("img");
-let container= document.getElementById("uno");
+
+
+var img = document.getElementById("img");
+var cont = document.getElementById("uno");
+img.addEventListener("dragend",cambiarColor);
+
+function cambiarColor(){
+    cont.style.background="yellow";
+}
 
 img.ondragstart= function(){
     console.log("Op");
