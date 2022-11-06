b5a20e4552109f32c0c48ad9372073e0c0f08c6d
mapaez13 <mapaez13@gmail.com>
2022-11-06 14:31:26 -0300

    modified:   Javascript/Repaso/index.html 	modified:   Javascript/Repaso/main.js

---
+++ Javascript/Repaso/main.js
@@ -0,0 +1,14 @@
+let uno=1;
+console.log("El valor de uno es " + uno + " y es numerico");
+
+var palabra="esclermatozoide";
+console.log("La palabra que escribimos en la variable palabra es " + palabra);
+
+array=["uno", 2, false, palabra];
+console.log("El arreglo es "+ array)
+
+objetos={
+    "opcion 1": "empanada",
+    "opcion 2": "frutas",
+    "opcion 3": "postre"
+}
